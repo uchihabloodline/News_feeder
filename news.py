b871f5c0e65bb0698557ffec1b182d84a999c832
gaara of sand <geetapandey595@gmail.com>
2018-05-23 00:15:43 +0530

    Update news.py

--- news.py
+++ news.py
@@ -2,9 +2,9 @@ import aylien_news_api
 from aylien_news_api.rest import ApiException
 
 # Configure API key authorization: app_id
-aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-ID'] = ' d31b61f1'
+aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-ID'] = '__your_key__ '
 # Configure API key authorization: app_key
-aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-Key'] = ' 88aa8cda7ec8c9e65611e7c463ca82f3'
+aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-Key'] = '__your_key__'
 
 # create an instance of the API class
 api_instance = aylien_news_api.DefaultApi()
